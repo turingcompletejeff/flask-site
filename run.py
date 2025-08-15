ec54d5aac9f8e68a2fffdd7c3a389848cda8e568
shades <daethstalker@gmail.com>
2025-08-15 15:31:30 -0400

    working on WSL

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
-from waitress import serve
 from app import create_app
 
+# Create an app instance
+app = create_app()
+
 if __name__ == '__main__':
-    # Create an app instance using the factory function
-    app = create_app()
-    
-    # Start the Waitress server
-    serve(app, host='0.0.0.0', port=80)
+    # Used to run the server manually (for local testing)
+    app.run(host='0.0.0.0', port=8000)
