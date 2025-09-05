b0fd9bf2e63bc53759e4e98fa09b9f9fa2263ce8
Jeff Rodgers <turingcompletejeff@gmail.com>
2025-09-04 23:15:45 -0400

    added a form tag and a rough outline of the controller fn

--- app/routes_blogpost.py
+++ app/routes_blogpost.py
@@ -11,3 +11,30 @@ def view_post(post_id):
     # Query the specific blog post by ID
     post = BlogPost.query.get_or_404(post_id)
     return render_template('view_post.html', post=post)
+
+# Route to create a new blog post
+@blogpost_bp.route('/new', methods=['POST'])
+@login_required
+def new_post():
+    form = BlogPostForm()
+    
+    # save portrait to uploads dir
+    
+    # downsize & save a thumbnail to the same dir
+    
+    # only commit the name(s) to the db...
+    
+    
+    if form.validate_on_submit():
+        # Create a new blog post object
+        post = BlogPost(
+            title=form.title.data,
+            content=form.content.data,
+            portrait=form.portrait.data # FIXME pull the name from the file data
+        )
+        # Add the new post to the database
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('main_bp.index'))
+    return render_template('new_post.html', form=form)
