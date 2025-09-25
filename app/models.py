0a24741446e053cf4afe879dbb397008298cfa24
Jeff Rodgers <turingcompletejeff@gmail.com>
2025-09-25 12:51:39 -0400

    adjustments for windows setup. updated reqs.

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
