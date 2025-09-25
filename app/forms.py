c34e957a5ad55bb63de881591c88ff9c93a25381
Jeff Rodgers <turingcompletejeff@gmail.com>
2025-09-25 14:15:54 -0400

    TC-6 added phone input with mask +time 15m

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SelectField, SubmitField
 from flask_wtf.file import FileField, FileAllowed
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, ValidationError
+import re
+
+class PhoneNumber:
+    def __init__(self, message=None):
+        if not message:
+            message = 'Invalid phone number format.'
+        self.message = message
+
+    def __call__(self, form, field):
+        if not field.data:
+            return
+
+        phone = re.sub(r'[^\d]', '', field.data)
+
+        if len(phone) != 10:
+            raise ValidationError(self.message)
+
+        if not re.match(r'^\d{10}$', phone):
+            raise ValidationError(self.message)
 
 class ContactForm(FlaskForm):
     name = StringField("name", validators=[DataRequired()])
     email = StringField("email", validators=[DataRequired(), Email()])
+    phone = StringField("phone number", validators=[PhoneNumber()])
     reason = SelectField(
         "reason for contact",
         choices=[
