e6381e88e26147d3342c7b7f47ae7cd6f300aaa6
jeff <turingcompletejeff@gmail.com>
2025-10-03 22:34:59 -0400

    TC-27 fixed import error

--- app/routes_admin.py
+++ app/routes_admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, abort, request, jsonify
+from flask import Blueprint, render_template, redirect, url_for, flash, abort, request, jsonify, current_app
 from flask_login import login_required, current_user
 from functools import wraps
 from app import db
