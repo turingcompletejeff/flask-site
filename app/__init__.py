4597d4f12bceb663b41f74b3b7350de81e6493ab
jeff <turingcompletejeff@gmail.com>
2025-10-07 11:30:46 -0400

    updated version

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 from config import Config
 from .filters import register_filters
 
-__version__ = "0.2.3"
+__version__ = "0.2.4" # initial testing fmwk
 
 db = SQLAlchemy()
 rcon = None
